eeae161f59819d1e6ad70e7584436a01fa02a73a
Eugene <evgeniys1@tut.by>
2019-01-22 16:09:38 +0300

    lesson 4(half)

---
+++ lesson4/my-app/src/components/post-list/post-list.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import PostListItem from '../post-list-item/';
+import './post-list.css';
+
+const PostList = ({posts}) => {
+
+    const elements = posts.map((item)=> {
+        const {id, ...itemProps} = item;
+
+        // if (itemProps == null) {
+
+        // } else {
+        //     return (
+        //         <li key={id} className='list-group-item'>
+        //             <PostListItem {...itemProps} />
+        //         </li>
+        //     )
+        // }
+
+        return (
+            <li key={id} className='list-group-item'>
+                <PostListItem {...itemProps} />
+                {/* label={item.label}
+                 important={item.important} */}
+            </li>
+        )
+    });
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    )
+}
+
+export default PostList;
